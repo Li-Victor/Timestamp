bfdab750b439f925dba2daeecc71e2129f32ae72
Victor Li <victorli5674@yahoo.com>
2017-08-08 13:20:51 -0700

    fixed for all other requests

--- index.js
+++ index.js
@@ -19,24 +19,7 @@ app.get('/:date', dateController.getDate);
 
 
 //all other Requests
-app.get('*', function (req, res, next) {
-    next();
-});
-
-app.post('*', function (req, res, next) {
-    next();
-});
-
-app.put('*', function (req, res, next) {
-    next();
-});
-
-app.delete('*', function (req, res, next) {
-    next();
-});
-
-//error middleware
-app.use(function (err, req, res, next) {
+app.all('*', function (req, res, next) {
     res.status(404).send({
         unix: null,
         natural: null
